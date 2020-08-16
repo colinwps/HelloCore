48e2d47f40ebdaa1d90f15c74438451f68b85a94
Colin <Colin@DESKTOP-3OPQMAC>
2020-08-16 09:38:02 +0800

    Add project files.

---
+++ HelloCore/Services/IClock.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloCore.Services
+{
+    public interface IClock
+    {
+    }
+}
