65561b15c83315db9c8dce6658f8f633ce8430ad
Colin <Colin@DESKTOP-3OPQMAC>
2020-08-18 22:27:25 +0800

    INIT

---
+++ HelloCore/Controllers/DepartmentController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HelloCore.Services;
+using HelloCore.Models;
+
+namespace HelloCore.Controllers
+{
+    public class DepartmentController:Controller
+    {
+        private readonly IDepartmentService _departmentService;
+        public DepartmentController(IDepartmentService departmentService)
+        {
+            _departmentService = departmentService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.Title = "部门首页";
+            var departments = _departmentService.GetAll();
+            return View(departments);
+        }
+
+        public IActionResult Add()
+        {
+            ViewBag.Title = "添加部门";
+            return View(new Department());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                await _departmentService.Add(department);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ HelloCore/Services/EmployeeService.cs
@@ -0,0 +1,46 @@
+﻿using HelloCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloCore.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly List<Employee> _employees = new List<Employee>();
+
+        public EmployeeService()
+        {
+            _employees.Add(new Employee { Id = 1, DepartmentId = 1, FirstName = "colin", LastName = "wang", Gender = Gender.男, Fired = false });
+            _employees.Add(new Employee { Id = 2, DepartmentId = 2, FirstName = "colin2", LastName = "wang", Gender = Gender.男, Fired = false });
+            _employees.Add(new Employee { Id = 3, DepartmentId = 3, FirstName = "colin3", LastName = "wang", Gender = Gender.男, Fired = false });
+        }
+
+        public Task Add(Employee employee)
+        {
+            employee.Id = _employees.Max(x => x.Id) + 1;
+            _employees.Add(employee);
+            return Task.CompletedTask;
+        }
+
+        public Task<Employee> Fire(int id)
+        {
+            return Task.Run(function:()=>
+            {
+                var employee = _employees.FirstOrDefault(t=>t.Id == id);
+                if (employee != null)
+                {
+                    employee.Fired = true;
+                    return employee;
+                }
+                return null;
+            });
+        }
+
+        public Task<IEnumerable<Employee>> GetByDepartmentId(int departmentId)
+        {
+            return Task.Run(function:()=>_employees.Where(t=>t.DepartmentId == departmentId).AsEnumerable());
+        }
+    }
+}
